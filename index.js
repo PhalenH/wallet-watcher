6ba6afc77205698b187c8c7ec46883f03b156ac6
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2022-05-27 16:14:16 -0400

    added logout button and more functionality to login/logout buttons

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 console.log("hello world");
 
-window.useWalletAddress = null
+window.userWalletAddress = null;
 
 const loginButton = document.getElementById("loginButton");
+const logoutButton = document.getElementById("logoutButton");
+
+const userWallet = document.getElementById("userWallet");
 
 function toggleButton() {
   if (!window.ethereum) {
@@ -12,20 +15,36 @@ function toggleButton() {
     return false;
   }
 
-  loginButton.addEventListener("click", loginWithMetaMask)
+  loginButton.addEventListener("click", loginWithMetaMask);
 }
 
 async function loginWithMetaMask() {
-   const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-   .catch((e)=> {
-       console.error(e.message)
-       return
-   })
-   const account = accounts[0];
-   window.useWalletAddress = account
+  const accounts = await window.ethereum
+    .request({ method: "eth_requestAccounts" })
+    .catch((e) => {
+      console.error(e.message);
+      return;
+    });
+  if (!accounts) {
+    return;
+  }
+  const account = accounts[0];
+  window.userWalletAddress = account;
+  userWallet.innerText = window.userWalletAddress;
+
+  loginButton.style.display = "none";
+  logoutButton.style.display = "flex";
+  logoutButton.addEventListener("click", signOutMetaMask);
+}
+
+async function signOutMetaMask() {
+  window.userWalletAddress = null;
+  userWallet.innerText = "";
+  logoutButton.style.display = "none";
+  loginButton.style.display = "flex";
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-    toggleButton()
+  toggleButton();
   console.log("DOM fully loaded and parsed");
 });
