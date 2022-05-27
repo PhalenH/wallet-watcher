7802e417f1d68eba552be6f2cff8ea58f0c635da
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2022-05-26 21:59:44 -0400

    minimal styling, initialized first function

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("hello world")
+
+const loginButton = document.getElementById("loginButton")
+
+function toggleButton() {
+    if (!window.ethereum) {
+        loginButton.innerText = "MetaMask is not installed"
+        loginButton.classList.remove()
+        loginButton.classList.add()
+    }
+}
