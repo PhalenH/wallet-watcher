c86f169b34beb526ebf4509f985c55362659fdf9
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2022-05-27 15:46:45 -0400

    added js logic for loggin in with metaMask

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-console.log("hello world")
+console.log("hello world");
 
-const loginButton = document.getElementById("loginButton")
+window.useWalletAddress = null
+
+const loginButton = document.getElementById("loginButton");
 
 function toggleButton() {
-    if (!window.ethereum) {
-        loginButton.innerText = "MetaMask is not installed"
-        loginButton.classList.remove()
-        loginButton.classList.add()
-    }
+  if (!window.ethereum) {
+    loginButton.innerText = "MetaMask is not installed";
+    loginButton.classList.remove("goodButton");
+    loginButton.classList.add("badButton");
+    return false;
+  }
+
+  loginButton.addEventListener("click", loginWithMetaMask)
 }
+
+async function loginWithMetaMask() {
+   const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+   .catch((e)=> {
+       console.error(e.message)
+       return
+   })
+   const account = accounts[0];
+   window.useWalletAddress = account
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    toggleButton()
+  console.log("DOM fully loaded and parsed");
+});
