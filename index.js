242fd2b15aada33adea2e2072a340338bd948812
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2022-05-31 18:49:47 -0400

    tried adding cdnjs for web 3 and connect it in index.js, still working on it

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+// const { default: Web3 } = require("web3");
+
 console.log("hello world");
+
 
 window.userWalletAddress = null;
 
@@ -7,6 +10,7 @@ const logoutButton = document.getElementById("logoutButton");
 
 const userWallet = document.getElementById("userWallet");
 
+// will gray out and display alternate message if metamask is not installed
 function toggleButton() {
   if (!window.ethereum) {
     loginButton.innerText = "MetaMask is not installed";
@@ -15,9 +19,11 @@ function toggleButton() {
     return false;
   }
 
+  // click event for when user does have metamask downloaded
   loginButton.addEventListener("click", loginWithMetaMask);
 }
 
+// gets accounts array
 async function loginWithMetaMask() {
   const accounts = await window.ethereum
     .request({ method: "eth_requestAccounts" })
@@ -28,15 +34,19 @@ async function loginWithMetaMask() {
   if (!accounts) {
     return;
   }
+  // Returns first account
   const account = accounts[0];
   window.userWalletAddress = account;
+  // displays user account address
   userWallet.innerText = window.userWalletAddress;
 
+  // displays logout button, removes display of login button
   loginButton.style.display = "none";
   logoutButton.style.display = "flex";
   logoutButton.addEventListener("click", signOutMetaMask);
 }
 
+// removes account text and logout button, displays login again
 async function signOutMetaMask() {
   window.userWalletAddress = null;
   userWallet.innerText = "";
@@ -44,7 +54,23 @@ async function signOutMetaMask() {
   loginButton.style.display = "flex";
 }
 
+// checks if window has MetaMask after dom loads
 window.addEventListener("DOMContentLoaded", () => {
   toggleButton();
   console.log("DOM fully loaded and parsed");
 });
+
+
+// // import web3 js
+const web3 = new Web3(window.ethereum)
+console.log("web3 loaded", web3)
+
+// // endpoint to get connected to ethereum node 
+// const web3 = new Web3("")
+
+// // private key to send transactions
+// web3.eth.accounts.wallet.add("")
+
+// // to get ether balance of an address
+// web3.eth.getBalance("")
+// .then(balance => console.log(balance))
